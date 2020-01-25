00e44c1176db68c2ae34fd7dc248c7df8c53d814
Alfred Ikiugu <ikiugualf@gmail.com>
2020-01-25 16:25:57 +0300

    added a new controller to pass form data between views

---
+++ WebContent/WEB-INF/view/helloworld.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello world input form response</title>
+</head>
+<body>
+	Hello spring member!!!
+	<br><br>
+	Student name: ${param.studentName}
+</body>
+</html>
---
+++ src/com/ikiugu/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.ikiugu.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String showOtherForm() {
+		return "helloworld";
+	}
+}

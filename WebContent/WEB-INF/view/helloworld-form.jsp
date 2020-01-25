4feec809ca86e461b7e690d88f19ec8da5e207da
Alfred Ikiugu <ikiugualf@gmail.com>
2020-01-25 17:07:14 +0300

    use requestParam instead of request.getparameter

--- WebContent/WEB-INF/view/helloworld-form.jsp
+++ WebContent/WEB-INF/view/helloworld-form.jsp
@@ -4,7 +4,7 @@
 	<title>Hello world input form</title>
 </head>
 <body>
-	<form action="processFormVersionTwo" method="get">
+	<form action=processFormVersionThree method="get">
 		<input type="text" name="studentName" placeholder="What's your name?" />
 		
 		<input type="submit" />
--- src/com/ikiugu/springdemo/mvc/HelloWorldController.java
+++ src/com/ikiugu/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -30,6 +31,24 @@ public class HelloWorldController {
 		
 		// create the message
 		String result = "Hey!!! " + theName;
+		
+		// add the message to the model
+		model.addAttribute("message", result);
+		
+		// return the message
+		return "helloworld";
+		
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String letsMakeNoise(@RequestParam("studentName") String theName, 
+			Model model) {
+		
+		// convert the data (uppercase)
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Make some noise for " + theName;
 		
 		// add the message to the model
 		model.addAttribute("message", result);
